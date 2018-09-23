4406b81fd287e3d4d7bf8b73470e55e95c1f76e7
Marcin Ziajkwski <ziajkowski.marcin@gmail.com>
2018-09-23 15:25:13 +0200

    Splited files
    Some functions are moved to another files. Fixed reading in Decriptor.

---
+++ Cpp-code/KonstKey/VarKey.h
@@ -0,0 +1,71 @@
+#ifndef _VARKEY_H_
+#define _VARKEY_H_
+
+#include <iostream>
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>		/* time */
+#include <math.h>
+#include <string>		/*std::to_string(int); */
+#include <random>
+
+#ifndef __uint8_t
+typedef unsigned char uint8_t;
+#define __uint8_t
+#endif __uint8_t
+
+using namespace std;
+
+//string FirstParameters();
+
+string FirstParameters()
+{
+	//conditions:
+	// p1 <1,100>
+	// p2 <2,10>
+	// p3 <1,1000>
+	// tconst <1,100>
+	// tvar <1,unsigned int - 1>
+	// offset <1,100> (co ile iteracji jest ta w³aœciwa)
+	
+
+	uint8_t p1,p2,tconst,offset;
+	short p3;
+	unsigned int tvar;
+	short divide = 1;
+
+	p1 = rand() % 100 + 1;
+
+	string ret_str = "#";
+	ret_str += to_string(p1);
+
+	p2 = rand() % 100 + 1;
+
+	ret_str += '#';
+	ret_str += to_string(p2);
+
+	p3 = rand() % 100 + 1;
+
+	ret_str += '#';
+	ret_str += to_string(p3);
+
+	tconst = rand() % 100 + 1;
+
+	ret_str += '#';
+	ret_str += to_string(tconst);
+
+	tvar = rand() % 100 + 1;
+
+	ret_str += '#';
+	ret_str += to_string(tvar);
+
+	offset = rand() % 100 + 1;
+
+	ret_str += '#';
+	ret_str += to_string(offset);
+
+	ret_str += '#';
+
+	return ret_str;
+}
+
+#endif _VARKEY_H_
--- Cpp-code/KonstKey/main.cpp
+++ Cpp-code/KonstKey/main.cpp
@@ -1,20 +1,22 @@
 // KonstKey.cpp : Defines the entry point for the console application.
 //
-
-
-
 #include "stdafx.h"
-
-
 #include <iostream>
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>		/* time */
+#include <math.h>
+#include <string>		/*std::to_string(int); */
+#include <random>
+#include "ConstKey.h"
+#include "VarKey.h"
+#include "Cezar3D.h"
+
+#ifndef __uint8_t
+typedef unsigned char uint8_t;
+#define __uint8_t
+#endif __uint8_t
 
 using namespace std;
-typedef unsigned char uint8_t;
-
-uint8_t * ConstKey(int,int);
-uint8_t functionCK(int);
-int silnia (int);
-
 
 int main()
 {
@@ -28,76 +30,47 @@ int main()
 	for(int i=0;i<s;i++) cout<<"["<<U[i]<<"]";
 	cout<<endl;
 	delete U;
-	
-	//tworzenie randomowych liczb ktÃ³re bedÄ… parametrami "123.234.45.6.456"
 
-	//dodaÄ‡ sumÄ™ kontrolnÄ… "123.234.45.6.456.99"
 
-	//dodac znaki konczÄ…ce (wypelniajace braki) Å¼eby wiadomosc zawsze miala stala dÅ‚ugoÅ›Ä‡
+	//tworzenie randomowych liczb które bed¹ parametrami "123.234.45.6.456"
+
+	//dodaæ sumê kontroln¹ "123.234.45.6.456.99"
+
+	//dodac znaki koncz¹ce (wypelniajace braki) ¿eby wiadomosc zawsze miala stala d³ugoœæ
 
 	//zapisanie ich jako Cezar 3D (Cezar->znak+x) (Cezar tablicowy->[znak1 to znak2][znak2 to znak3][znakN to znak(N+1) ] )
 	// (Cezar macierzowy -> znak1 to znak2 lub znak3 lub znak4 ...)
-	// Cezar 3D to Cezar macierzowy dla pierwszego znaku, CM->znak1, na podstawie wyboru znaku zastÄ™pczego dla nastÄ™pnego znaku rozkodowywanego
-	// wybierana jest kolejna inna macierz wyboru dla znakÃ³w. 
+	// Cezar 3D to Cezar macierzowy dla pierwszego znaku, CM->znak1, na podstawie wyboru znaku zastêpczego dla nastêpnego znaku rozkodowywanego
+	// wybierana jest kolejna inna macierz wyboru dla znaków. 
 
-	//naÅ‚oÅ¼yÄ‡ klucz statyczny (ConstKey)
+	//na³o¿yæ klucz statyczny (ConstKey)
 
 	//Gotowe mozna wysylac
 
-	//sprawdzic czy wiadomosc ma odp. dÅ‚ugoÅ›Ä‡.
+	//sprawdzic czy wiadomosc ma odp. d³ugoœæ.
 
-	//sciÄ…gnÄ…Ä‡ klucz ststyczny ~(ConstKey)
+	//sci¹gn¹æ klucz ststyczny ~(ConstKey)
 
-	//rozszyfrowac cezara (Cezar 3D moÅ¼na debugowaÄ‡, sprawdziÄ‡ czy nastÄ™pny znak jest w ogÃ³le dopuszczalny)
-	// kaÅ¼dy znak a:z i 0:9 i # to 38 znakÃ³w. mogÄ… mieÄ‡ maksymalnie 6 moÅ¼liwoÅ›ci i 0 oraz 255 sÄ… zakazane. 
+	//rozszyfrowac cezara (Cezar 3D mo¿na debugowaæ, sprawdziæ czy nastêpny znak jest w ogóle dopuszczalny)
+	// ka¿dy znak a:z i 0:9 i # to 38 znaków. mog¹ mieæ maksymalnie 6 mo¿liwoœci i 0 oraz 255 s¹ zakazane. 
 
-	//sprawdzic poprawnosc wiadomosci: czy sÄ… wszystkie paramtetry, czy majÄ… odpowiedni rozmiar, czy sÄ… same liczby i #, czy jest suma kontrolna
+	//sprawdzic poprawnosc wiadomosci: czy s¹ wszystkie paramtetry, czy maj¹ odpowiedni rozmiar, czy s¹ same liczby i #, czy jest suma kontrolna
 
-	//zsumowac wiadomosc i porÃ³wnac z suma kontrolnÄ… 
+	//zsumowac wiadomosc i porównac z suma kontroln¹ 
 
-	//jezeli wszystko przebiegÅ‚o bez bÅ‚Ä™dÃ³w stworzyc klucz na podstawie parametrow podanych w wiadomosci
+	//jezeli wszystko przebieg³o bez b³êdów stworzyc klucz na podstawie parametrow podanych w wiadomosci
+	
+	
+	
 
+	string heja;
+	heja = Cezar3D_Encrypt("abcdd",Keytab);
+	cout<<heja<<"\n";
+	string ho;
+	ho = Cezar3D_Decrypt(heja,Keytab);
+	cout<<ho<<"\n";
 
 	cin.get();
     return 0;
-}
-
-uint8_t * ConstKey(int start,int stop)
-{
-    if (start<0 || stop<=start || stop > 128) throw "Wrong input in ConstKey function"; //0x00 57 a6 d8
-    int Size = (stop-start)+1;
-    uint8_t * ret = new uint8_t[Size];
-	
-    for(int i=0;i<Size;i++)
-    {
-        ret[i] = functionCK(start++);
-    }
-	
-    return ret;
-}
-
-uint8_t functionCK(int t)
-{
-	uint8_t ret=0xFF;
-
-	if(t<25 || t>100)
-	{
-		ret = silnia(t%5);
-		ret = ret * t - t*t + (ret/2);
-	}
-	else
-	{
-		ret = silnia(t%10);
-		ret = ret + 2*t - 4;
-	}
-	
-    return ret==0 ? ret-1 : ret;
-}
-
-
-int silnia (int liczba)
-{
-    if (liczba<2) return liczba;
-    return liczba*silnia(liczba-1);
 }
 
