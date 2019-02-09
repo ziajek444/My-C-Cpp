8d9c9c659dd30d4d5041ec82805d193d6f355706
ziajek444 <yinyang69@wp.pl>
2019-02-09 19:14:13 +0100

    make that working dynamic\r\nNow I have to implement new feartures\r\nThe project is more useful than :any or std::auyt variables right now, and in futher will be even better

--- dynamic var c++/dynamic/main.cpp
+++ dynamic var c++/dynamic/main.cpp
@@ -6,10 +6,12 @@
 #include <string.h>
 #include <stdint.h>
 #include "AbstractClass.h"
+#include <memory>
 
 
 #define TYPE(x) typeid(x).name()
 
+#define __POINTERTYPE 1
 
 template <class Type>
 void print(const Type p);
@@ -18,74 +20,183 @@ void println(const Type p);
 
 //------------------------------------------------- dynamic
 
-template <typename T4, typename T5>
-void printToConsole(T4 typ, T5 wartosc)
+template <typename T1, typename T2>
+void printToConsole(T1 typ, T2 wartosc)
 {
-	std::cout << static_cast<T4>(wartosc);
+	std::cout << static_cast<T1>(wartosc);
 }
 
 class dynamic
 {
-	// type erasure
-	
-private:
-	struct ObjectConcept {
-	public:
-		virtual void T_print_() const = 0;
-		virtual ~ObjectConcept() {}
 
-	};
-
-	template< typename T > struct ObjectModel : ObjectConcept {
-	public:
-		ObjectModel(const T& t) : object(t) {}
-		virtual void T_print_() const
-		{
-			printToConsole(object, object);
-		}
-		virtual ~ObjectModel() {}
+#pragma region dynamic type erasure
 	private:
-		T object;
-	};
-	//--------------------------------------------------------
+		struct ObjectConcept {
+			public:
+				virtual void PrintFromDynmic() const = 0;
+				virtual ~ObjectConcept() {}
+		};
 
+#if __POINTERTYPE == 1
+		template< class T3 > 
+		struct ObjectModel : ObjectConcept {
+			public:
+				
+				ObjectModel(const T3& t)  {
+					//int size = sizeof(t) / sizeof(T3);
+					//object = new T3[size];
+					object = const_cast<T3*>(&t);
+					IsNeedToDeallocate = false;
+				}
+				ObjectModel(const T3&& t)  {
+					object = new T3[1];
+					object[0] = t;
+					IsNeedToDeallocate = true;
+				}
+				virtual void PrintFromDynmic() const
+				{
+					printToConsole(*object, *object);
+				}
+				virtual ~ObjectModel() {
+					if(IsNeedToDeallocate)
+						delete[] object;
+				}
+			private:
+				T3 * object;
+				int IsNeedToDeallocate;
+		};
+#else
+		template< class T3 >
+		struct ObjectModel : ObjectConcept {
+		public:
+			ObjectModel(const T3& t) {
+				object = t;
+			}
+			ObjectModel(const T3&& t) {
+				object = t;
+			}
+			virtual void PrintFromDynmic() const
+			{
+				printToConsole(object, object);
+			}
+			virtual ~ObjectModel() {}
+		private:
+			T3 object;
+		};
+#endif
+		
+#pragma endregion
+
+#pragma region ctors
 public:
-	dynamic() : initialized(false), data_type(typeid(void))
+	dynamic() : self_ID(++this->ID), data_type(typeid(void))
 	{
-		this->ID += 1;
-		this->self_ID = this->ID;
+		initialized = false;
+		var_size = type_size = 0;
+	}
+	dynamic(dynamic& dynamic_in) : self_ID(++this->ID), data_type(typeid(void))
+	{
+		std::cout << "dyn ass\r\n";
+		DYNAMIC = dynamic_in.getObject();
+
+		initialized = dynamic_in.IsInitialized();
+		var_size    = dynamic_in.getVarSize();
+		type_size   = dynamic_in.getTypeSize();
+	}
+	template <typename T4>
+	dynamic(const T4& obj_in) : self_ID(++this->ID), data_type(typeid(T4)), DYNAMIC(new ObjectModel<T4>(obj_in))
+	{
+		initialized = true;
+		var_size    = sizeof(obj_in);
+		type_size   = sizeof(T4);
+	}
+	template <typename T5>
+	dynamic(const T5&& obj_in_r) : self_ID(++this->ID), data_type(typeid(T5)), DYNAMIC(new ObjectModel<T5>(obj_in_r))
+	{
+		initialized = true;
+		var_size    = sizeof(obj_in_r);
+		type_size   = sizeof(T5);
 	}
 
-	int getID()
-	{
-		return this->self_ID;
-	}
+#pragma endregion
 
-	template <class Temp>
-	dynamic & operator= (const Temp & x)
-	{
+#pragma region Self functions
+	public:
+		const int getID() const    { return this->self_ID;     }
+		bool IsInitialized() const { return this->initialized; }
+		int getVarSize() const     { return this->var_size;    }
+		int getTypeSize() const    { return this->type_size;   }
+		std::shared_ptr<ObjectConcept> getObject() const
+		                           { return this->DYNAMIC;     } 
+#pragma endregion
 
-		return *this;
-	}
+#pragma region override functions
+	private:
+		void PrintFromDynmic() const
+		{
+			(this->DYNAMIC)->PrintFromDynmic();
+		}
+#pragma endregion
 
-	template <class Temp>
-	dynamic & operator =(const Temp && x)
-	{
-		return *this;
-	}
+#pragma region assigns
+	public:
+		dynamic & operator= (const dynamic& dynamic_in)
+		{
+			this->DYNAMIC = dynamic_in.getObject();
 
-	friend std::ostream & operator<< (std::ostream & out, const dynamic & am) {}
+			initialized = dynamic_in.IsInitialized();
+			var_size = dynamic_in.getVarSize();
+			type_size = dynamic_in.getTypeSize();
 
-	virtual ~dynamic() { }
+			return *this;
+		}
+		template <class Temp>
+		dynamic & operator= (const Temp & x) 
+		{
+			data_type = typeid(Temp);
+			DYNAMIC = std::make_shared<ObjectModel<Temp>>(x); //new ObjectModel<Temp>(x);
+			initialized = true;
+			var_size = sizeof(x);
+			type_size = sizeof(Temp);
+			return *this;
+		}
+		template <class Temp>
+		dynamic & operator =(const Temp && x) 
+		{
+			data_type = typeid(Temp);
+			DYNAMIC = std::make_shared<ObjectModel<Temp>>(x); //new ObjectModel<Temp>(x);
+			initialized = true;
+			var_size = sizeof(x);
+			type_size = sizeof(Temp);
+			return *this;
+		}
+#pragma endregion
 
+#pragma region OSTREAM !!
+	// There is the real powerfull of my work. No one can do that ! (not even ::any)
+	public:
+		friend std::ostream & operator<< (std::ostream & out, const dynamic & am) {
+			am.PrintFromDynmic();
+			return out;
+		}
+#pragma endregion
+
+#pragma region Dtor
+	public:
+		virtual ~dynamic() { }
+#pragma endregion
+
+#pragma region variables
 private:
 	static int ID;
-	int self_ID;
-	std::unique_ptr<ObjectConcept> object;
+	const int self_ID;
+	std::shared_ptr<ObjectConcept> DYNAMIC;
 	bool initialized;
-	int var_size;
-	int item_size;
 	std::type_index data_type;
+	int var_size;
+	int type_size;
+	
+#pragma endregion
 
 };
 
@@ -94,9 +205,31 @@ private:
 int main()
 {
 	
-	
+	dynamic d = 4;
+	dynamic dyn = d;
+	std::cout << "=> 4_/4_ \r\n";
+	std::cout << d << std::endl;
+	std::cout << dyn << std::endl;
+	d = 44;
+	std::cout << "=> 44/4_ \r\n";
+	std::cout << d << std::endl;
+	std::cout << dyn << std::endl;
+	dyn = d;
+	std::cout << "=> 44/44 \r\n";
+	std::cout << d << std::endl;
+	std::cout << dyn << std::endl;
+
+	//d = (std::string)"stringers"; // ok
+	//d = (const char *)"stringers"; // ok
+	d = "stringers";
+	std::cout <<"\r\n"<< d << std::endl;
+
+	std::string s = "slowo";
+	d = s;
+	std::cout << "\r\n" << d << std::endl;
 
 	std::cin.get();
+
 	return 0;
 
 }
